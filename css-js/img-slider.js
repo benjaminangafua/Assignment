523abc5184d0c14015042d85508acbd196a27e47
benjaminangafua <benjaminarkutl2017@gmail.com>
2023-08-29 17:43:50 +0000

    Implemented image slider

--- css-js/img-slider.js
+++ css-js/img-slider.js
@@ -1,6 +1,7 @@
 const body = document.querySelector("body")
 const imgDiv = createElement("div")
 const img = createElement("img")
+
 imgDiv.append(img)
 body.append(imgDiv)
 
@@ -10,14 +11,67 @@ function createElement(ele) {
 let imgIndex = 0
 
 const imgs = [
-    "./quoters-img/benjamin.png",
-    "./quoters-img/james.png",
-    "./quoters-img/boakai.png"
-]
+    { href: "./img/benjamin.png" },
+    { href: "./img/james.png" },
+    { href: "./img/boakai.png" }
+];
+const icons = [
+    { left: "./img/left-arrow.png" },
+    { right: "./img/right-arrow.png" }
+];
+const slideImg = () => img.src = imgs[Math.floor(Math.random() * imgs.length)].href;
 
-const slideImg = () => img.src = imgs[Math.floor(Math.random() * imgs.length)]
-
-
-slideImg()
+slideImg();
 
 setInterval(slideImg, 2000)
+
+const createAttribute = (elem, src) => elem.setAttribute("src", src);
+
+const rightArr = createElement('img');
+const leftArr = createElement('img');
+
+console.log("Image");
+
+(() => {
+    const div = createElement('div')
+
+    div.setAttribute("class", "arrows")
+    createAttribute(rightArr, icons[1].right)
+    createAttribute(leftArr, icons[0].left)
+
+    div.append(leftArr, rightArr)
+    imgDiv.append(div)
+    imgDiv.setAttribute("class", "slider-container")
+
+    /**
+     * Click left - to change an image
+     *  *If index is greater than 0 
+     *  *   *decrement the index
+     *  *   *Set the image to the current index
+     * Click right - to change an image
+     *  *If index is less than image.length - 1
+     *  *   *Increment the index
+     *  *   *Set the image to the current index
+     */
+    leftArr.addEventListener("click", () => {
+        if (imgIndex > 0) {
+            imgIndex--
+            img.src = imgs[imgIndex].href
+        } else if (imgIndex == 0) {
+            imgIndex = imgs.length - 1
+            img.src = imgs[imgIndex].href
+        }
+        console.log("Right")
+    });
+
+    rightArr.addEventListener("click", () => {
+        imgIndex++
+        if (imgIndex < imgs.length) {
+            img.src = imgs[imgIndex].href
+
+        } else {
+            imgIndex = 0
+            img.src = imgs[imgIndex].href
+        }
+    });
+})()
--- css-js/watch.js
+++ css-js/watch.js
@@ -1,1 +1,49 @@
-const counter = document.getElementById("counter")
+const minutes = document.getElementById("minutes"),
+    seconds = document.getElementById("seconds"),
+    milliseconds = document.getElementById("milliseconds"),
+    restart = document.querySelector(".restart"),
+    pause = document.querySelector(".pause"),
+    start = document.querySelector(".start");
+let timeOut, sec, min, miSec;
+
+miSec = sec = min = 0;
+
+start.addEventListener("click", () => {
+    timeOut = setInterval(increment, 10)
+        // console.log(miSec)
+
+})
+
+function increment() {
+    miSec++
+    if (miSec === 100) {
+        ++sec
+        miSec = 0
+
+        if (sec == 60) {
+            ++min
+            sec = 0
+            miSec = 0
+            minutes.innerHTML = min < 10 ? `0${min} : ` : `${min} : `
+
+            if (min == 3) {
+                clearTimeout(timeOut)
+            }
+        }
+        seconds.innerHTML = sec < 10 ? `0${sec}  : ` : `${sec} : `
+
+    }
+    milliseconds.innerHTML = miSec < 10 ? `0${miSec}` : miSec
+
+}
+pause.addEventListener("click", () => {
+    clearTimeout(timeOut)
+
+})
+
+restart.addEventListener("click", () => {
+    sec = miSec = 0
+    minutes.innerHTML = "00 : "
+    seconds.innerHTML = "00 : "
+    milliseconds.innerHTML = "00"
+})
